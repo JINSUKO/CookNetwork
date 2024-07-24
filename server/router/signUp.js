5ee44889480f8e91cd6a1faff9c0037208528a9e
고진수 <rhwkwk789@gmail.com>
2024-07-24 23:35:53 +0900

    signUp 변경 원복

--- server/router/signUp.js
+++ server/router/signUp.js
@@ -16,18 +16,4 @@ router.post('/', async(req,res)=>{
     }
     
 })
-
-router.get('/', async(req,res)=>{
-    const { id, pw, name, sex, email } = req.body;
-
-    const queryString = `INSERT INTO users(user_id, user_password, username, sex, email) VALUES(?, ?, ?, ?, ?)`
-
-    try{
-        await maria.execute(queryString,[id,pw,name,sex,email]);
-        console.log("회원가입 성공!")
-    } catch(error){
-        console.error(error);
-    }
-
-})
 module.exports = router;
