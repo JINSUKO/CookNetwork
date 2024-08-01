d8d503b14eba9132e334c1d136b8ef6b35060c94
ASDF110724 <twento157@gmail.com>
2024-08-01 09:51:13 +0900

    채팅 추가사항

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,16 @@
 import React, { useState, useEffect } from 'react'
 import './App.css'
 import SignUp from "./pages/SignUp.jsx";
+import Chat from "./chat/chatIndex.jsx";
 
 function App() {
-  const [message, setMessage] = useState('&nbsp')
-
-  useEffect(() => {
-    const API_URL = import.meta.env.PROD
-      ? ''
-      : 'http://localhost:3000';
-
-    fetch(`${API_URL}/hello`)
-      .then(response => response.json())
-      .then(data => setMessage(data.message))
-      .catch(error => console.error('Error:', error));
-  }, [])
-
+  
   return (
     <div className="App">
       {/*<h1>React App</h1>*/}
       {/*<p>{message}</p>*/}
-      <SignUp/>
+      {/* <SignUp/> */}
+      <Chat/>
     </div>
   )
 }
---
+++ client/src/chat/chatIndex.jsx
@@ -0,0 +1,114 @@
+import { forwardRef, useEffect, useRef, useState } from 'react';
+import { Socket, io } from 'socket.io-client';
+
+import { StyledApp } from './styled.jsx';
+
+const{
+  Container,
+  Form,
+  HistoryWrapper,
+  NoHistory,
+  ChatItem,
+  ChatName,
+  ChatMessage,
+  NameInput,
+  MessageInput,
+  SubmitButton
+} = StyledApp;
+
+const socket = new io('http://localhost:3001/');
+
+function chatIndex() {
+  const [messageHistory, setMessageHistory] = useState([]);
+
+  const uid = useRef(null);
+  const historyElement = useRef(null);
+
+  const [userMessage, setUserMessage] = useState({
+    name: '김주현',
+    message: '',
+  });
+
+  useEffect(() => {
+    const onConnect = () =>{
+      uid.current = socket.id;
+    };
+    const onMessage = (data) =>{
+      console.log(`${data.id}: `,data);
+      
+      setMessageHistory((prevHistory) => [...prevHistory, { id: uid.current, ...data}]);
+    };
+    const onUserEnter = (userId) =>{
+      console.log(`${userId} 접속`);
+    };
+    const onUserLeave = (userId) =>{
+      console.log(`${userId} 접속 해제`);
+    };
+
+    socket.on('connect', onConnect);
+    socket.on('Message', onMessage);
+    socket.on('USER_ENTER', onUserEnter);
+    socket.on('USER_LEAVE', onUserLeave);
+  }, []);
+
+  useEffect(() => {
+    if(historyElement.current){
+    historyElement.current.scrollTop = historyElement.current.scrollHeight;
+    }
+  }, [messageHistory]);
+
+  const handleChange = (event) =>{
+    setUserMessage((prevState) => ({
+      ...prevState,
+      [event.target.id]: event.target.value,
+    }));
+  };
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    const messageToSend = { id: uid.current, ...userMessage };
+
+    socket.emit('Message', messageToSend);
+
+    setMessageHistory((prevHistory) => [...prevHistory, messageToSend]);
+
+    setUserMessage((prevUser) => ({
+      ...prevUser,
+      message: '',
+    }));
+  };
+  
+  return (
+    <Container>
+      <HistoryWrapper ref={historyElement}>
+        {messageHistory.length === 0 ? (
+          <NoHistory>첫 메시지를 남겨보세요🥳</NoHistory>
+        ) : (
+          messageHistory.map(({ id, name, message }, index) => (
+            <ChatItem key={index} me={id === socket.id}>
+              <ChatName>{name}</ChatName>
+              <ChatMessage>{message}</ChatMessage>
+            </ChatItem>
+          ))
+        )}
+      </HistoryWrapper>
+      <Form onSubmit={handleSubmit}>
+        <NameInput
+          id="name"
+          value={userMessage.name}
+          onChange={handleChange}
+          placeholder="닉네임"
+        />
+        <MessageInput
+          id="message"
+          value={userMessage.message}
+          onChange={handleChange}
+          placeholder="메시지를 입력하세요..."
+        />
+        <SubmitButton>보내기</SubmitButton>
+      </Form> 
+    </Container>
+  );
+}
+
+export default chatIndex;
