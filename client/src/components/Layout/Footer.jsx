f7ebf198acbfdea16d20c9913e5c8f6f9411a16e
dhkim <dhkm201@gmail.com>
2024-08-02 15:45:45 +0900

    Footer 글씨크기 변경 SignUp.jsx 회원가입 페이지. 회원가입 성공후 alert 추가, 에러메시지 위치 변경

--- client/src/components/Layout/Footer.jsx
+++ client/src/components/Layout/Footer.jsx
@@ -4,9 +4,8 @@ function Footer() {
 
   return ( 
     <header>
-      <h1>Footer 영역</h1>
-      <h2>Copyright ⓒ 2024 Cook Network 팀<br />
-      이 프로젝트는 코드랩아카데미의 교육 과정의 일환으로 개발되었습니다.</h2>
+      <p>Copyright ⓒ 2024 Cook Network 팀<br />
+      이 프로젝트는 코드랩아카데미의 교육 과정의 일환으로 개발되었습니다.</p>
     </header>
   )
 }
--- client/src/components/SearchResult.jsx
+++ client/src/components/SearchResult.jsx
@@ -1,4 +1,4 @@
-/* SearchResults.jsx
+/* SearchResult.jsx
 검색창 컴포넌트 중 부모 컴포넌트에 해당합니다.
 
 */
