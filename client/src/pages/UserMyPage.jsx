a8b8dd2dc31614bf21e5acf1742d514f670fc87c
고진수 <rhwkwk789@gmail.com>
2024-08-02 15:11:41 +0900

    유저 닉네임 수정 모달 추가, 서버쪽 수정 기능은 구현 중.

--- client/src/pages/UserMyPage.jsx
+++ client/src/pages/UserMyPage.jsx
@@ -4,7 +4,7 @@
 import {useState, useRef, useEffect} from 'react';
 import {Image, Container, Row, Col, Nav, InputGroup, FormControl, Button, Card, Modal} from 'react-bootstrap';
 
-const UserPage = ({user, profilePic}) => {
+const UserMyPage = ({user, profilePic}) => {
 
     const API_URL = 'http://localhost:3000';
 
@@ -32,7 +32,7 @@ const UserPage = ({user, profilePic}) => {
         reader.readAsDataURL(file);
 
 
-        setShowDialog(true)
+        setShowImgConfirmModal(true)
 
 
     };
@@ -42,43 +42,13 @@ const UserPage = ({user, profilePic}) => {
         fileInputRef.current.click();
     };
 
-    // 마이페이지에서 유저가 등록한 선호 카테고리 목록 데이터 요청 코드.
-    const getUserCategories = async () => {
-        try {
-            const response = await fetch(`${API_URL}/api/userCategories`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({user_code: user.user_code})
-            });
 
-            if (!response) throw new Error((await response.json().error));
 
-            const result = await response.json();
+    // 프로필 이미지 변경 확인 대화상자 부분 시작
+    const [showImgConfirmModal, setShowImgConfirmModal] = useState(false);
 
-            console.log(result);
-
-            setCategories(result);
-
-        } catch (e) {
-            console.error('Error:', e);
-        }
-    }
-
-    useEffect(() => {
-
-        // 마이페이지에서 유저가 등록한 선호 카테고리 목록 데이터 요청 코드.
-        getUserCategories();
-
-    }, []);
-
-    // 확인 대화상자 부분 시작
-    const [showDialog, setShowDialog] = useState(false);
-
-    const handleConfirm = async () => {
-        console.log('User confirmed');
-        setShowDialog(false);
+    const uploadProfileConfirm = async () => {
+        setShowImgConfirmModal(false);
         // 파일 저장 formData 객체 생성
         const formData = new FormData();
         console.log('fetch 직전 file:', profileImg)
@@ -106,9 +76,9 @@ const UserPage = ({user, profilePic}) => {
         }
     };
 
-    const handleCancel = () => {
-        console.log('User canceled');
-        setShowDialog(false);
+    const uploadProfileCancel = () => {
+        console.log('프로필 사진 업로드 취소');
+        setShowImgConfirmModal(false);
         setProfileImgDB(profilePic);
     };
 
@@ -130,7 +100,115 @@ const UserPage = ({user, profilePic}) => {
             </Modal>
         );
     };
-    // 확인 대화상자 부분 끝
+    // 프로필 이미지 변경 확인 대화상자 부분 끝
+
+    // 마이페이지에서 유저가 등록한 선호 카테고리 목록 데이터 요청 코드.
+    const getUserCategories = async () => {
+        try {
+            const response = await fetch(`${API_URL}/api/userCategories`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({user_code: user.user_code})
+            });
+
+            if (!response) throw new Error((await response.json()).error);
+
+            const result = await response.json();
+
+            console.log("유저의 카테고리 목록 호출 성공!");
+
+            setCategories(result);
+
+        } catch (e) {
+            console.error('Error:', e);
+        }
+    }
+
+    useEffect(() => {
+
+        // 마이페이지에서 유저가 등록한 선호 카테고리 목록 데이터 요청 코드.
+        getUserCategories();
+
+    }, []);
+
+    // 닉네임 수정 기능 시작
+    const [showUserNameModal, setShowUserNameModal] = useState(false);
+    const [checkConfirm, setCheckConfirm] = useState(true);
+
+    const usernameConfirm = async () => {
+        setShowUserNameModal(false);
+
+        try {
+            const response = await fetch(`${API_URL}/api/userNameUpdate`, {
+                method: 'PUT',
+                heather: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify()
+            })
+
+            if (!response) throw new Error((await response.json()).error);
+
+            const result = await response.json();
+
+            console.log('username 업데이트 성공!');
+
+
+        } catch (e) {
+            console.log('Error:', e);
+        }
+    };
+
+    const usernameCancel = () => {
+        console.log('유저 닉네임 변경 취소');
+        setShowUserNameModal(false);
+        setProfileImgDB(profilePic);
+    };
+
+    const UserNameModal = ({ show, preUsername, setCheckConfirm, onConfirm, onCancel }) => {
+
+        const [username, setUsername] = useState(preUsername);
+        const [usernameError, setUsernameError] = useState('');
+
+
+        const getUsernameEventListener = () => {
+            const regNickname = /^[a-zA-Z가-힣]{2,16}$/;
+
+            // 닉네임
+            if (!regNickname.test(username)) {
+                setUsernameError("닉네임은 한글 또는 영문 2~16자로 작성하세요.");
+                setCheckConfirm(false);
+            } else {
+                setUsernameError('');
+                setCheckConfirm(true);
+            };
+        }
+
+        return (
+            <Modal show={show} onHide={onCancel} centered>
+                <Modal.Header closeButton>
+                    <Modal.Title>Confirm Action</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    변경 전: {username}
+                    <br/>
+                    변경 후: <input type='text' defaultValue={''} onChange={getUsernameEventListener}/ >
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="primary" onClick={usernameConfirm}>
+                        Confirm
+                    </Button>
+                    <Button variant="secondary" onClick={usernameCancel}>
+                        Cancel
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        );
+    };
+    // 닉네임 수정 기능 끝
+
 
     return (
         <Container fluid className="p-3">
@@ -170,14 +248,21 @@ const UserPage = ({user, profilePic}) => {
                                         사진 변경하기
                                     </Button>
                                     <ImgConfirmModal
-                                        show={showDialog}
+                                        show={showImgConfirmModal}
                                         message="선택된 사진으로 변경하려면 확인을 눌러주세요."
-                                        onConfirm={handleConfirm}
-                                        onCancel={handleCancel}
+                                        onConfirm={uploadProfileConfirm}
+                                        onCancel={uploadProfileCancel}
                                     />
                                 </>
                             }
-                            <Button variant="dark" size="sm" className="mb-2">닉네임 수정하기</Button>
+                            <Button variant="dark" size="sm" className="mb-2" onClick={() => {setShowUserNameModal(true)}} >닉네임 수정하기</Button>
+                            <UserNameModal
+                                show={showUserNameModal}
+                                preUsername={user.username}
+                                setCheckConfirm={setCheckConfirm}
+                                onConfirm={usernameConfirm}
+                                onCancel={usernameCancel}
+                            />
                         </div>
                         <Row className="justify-content-center g-2 mb-3">
                             <Col>
@@ -235,7 +320,7 @@ const UserPage = ({user, profilePic}) => {
                                         이메일: {user.email}<br />
                                         유저 등급: {user.user_code <= 10 ? "운영자 계정" : (user.chef_code ? "셰프 계정" : "일반 계정")}
                                     </Card.Text>
-                                    <Button variant="dark" size="sm" className="mb-2">화원 정보 수정하기</Button>
+                                    <Button variant="dark" size="sm" className="mb-2">회원 정보 수정하기</Button>
                                     <div style={{marginTop:"30px"}}></div>
                                     <h6 className="mb-3">카테고리 찜 목록</h6>
 
@@ -318,4 +403,4 @@ const UserPage = ({user, profilePic}) => {
     );
 };
 
-export default UserPage;
+export default UserMyPage;
