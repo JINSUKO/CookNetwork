45eca5042e2847af6fd1888814cc8d3be639ab35
doy00 <164746857+doy00@users.noreply.github.com>
2024-08-02 21:25:15 +0900

    기본 css 내용 삭제 (#27)
    * Fdsfsd
    
    * fdsf
    
    * client/src
    
    * 완료: Navbar 페이지 이동 링크로 구현.
    RecipeDetail, RecipeList 대략적인 틀 작성.
    SearchBar 부트스트랩 사용 대략적인 틀 작성.
    Header 로고, 내비바 합치기. 페이지 이동시 상단 고정
    예정: 회원가입 에러메시지 배치 수정,
    
    * 회원가입, 로그인 기능 마무리 작업중
    
    * FilteredResult.jsx 추가(검색창 키워드 필터링 함수 컴포넌트)
    SearchResult.jsx 추가(검색 결과 화면 출력 함수)
    NavElement: className css 관련 수정
    SearchBar.jsx 전반적인 수정
    login.jsx: data 변수 선언, general 오타수정
    
    * 로그인 성공
    
    * Footer 글씨크기 변경
    SignUp.jsx 회원가입 페이지. 회원가입 성공후 alert 추가, 에러메시지 위치 변경
    
    * Login.jsx: 로그인 성공시 메인페이지로 이동
    SignUp.jsx: 회원가입 성공시 로그인 페이지로 이동
    
    * - 상단 네비게이션 바 검색창 내 카테고리 드롭다운 메뉴 추가
    - 드롭다운 메뉴 css 추가
    - 충돌을 유발하는 불필요한 App.css, index.css 주석처리
    -Searchbar.jsx 내 form 태그를 부트스트랩 Form 태그로 변경
    
    * 기본 css 내용 삭제
    
    ---------
    
    Co-authored-by: 고 진수(JINSU KO) <99123637+JINSUKO@users.noreply.github.com>

--- client/src/components/Layout/Header.jsx
+++ client/src/components/Layout/Header.jsx
@@ -1,15 +1,12 @@
 import React from "react";
 import logo from '../../assets/images/logo.png';
-import SearchBar from '../SearchBar.jsx';
 import NavbarElement from "../NavbarElement.jsx";
 
 function Header() {
 
   return ( 
     <header>
-      <h1>Header 영역</h1>
       <img src={logo} alt="로고 이미지" width={200}/>
-      <SearchBar/>
       <NavbarElement />
     </header>
   )
--- client/src/components/NavbarElement.jsx
+++ client/src/components/NavbarElement.jsx
@@ -7,6 +7,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import '../assets/styles/Navbar.css'
 import { Container, Nav, Navbar, Form, Button } from 'react-bootstrap';
+import SearchBar from './SearchBar';
 
 
 function NavbarElement() {
@@ -27,23 +28,15 @@ function NavbarElement() {
 
       {/* 헤더 아래 네비바 */}
       <Navbar expand="lg" className="bg-body-tertiary">
-        <Container className="d-flex justify-content-between align-items-center flex-nowrap">
-          <Navbar.Brand href="/" bg="light" data-bs-theme="light" className="flex-shrink-0">전체</Navbar.Brand>
-          <Nav className="mx-auto d-flex justify-content-center flex-grow-1 flex-shrink-0" >
+        <Container className="d-flex justify-content-between align-items-center flex-nowrap ">
+          <Nav className="mx-auto d-flex justify-content-center flex-grow-1 flex-shrink-0" style={{ overflow: 'visible' }}>
+            <Navbar.Brand href="/" bg="light" data-bs-theme="light" className="flex-shrink-0">전체</Navbar.Brand>
             <Nav.Link href="/korean" className="px-2">한식</Nav.Link>
             <Nav.Link href="/western" className="px-2">양식</Nav.Link>
             <Nav.Link href="/chinese" className="px-2">중식</Nav.Link>
             <Nav.Link href="/japanese" className="px-2">일식</Nav.Link>
           </Nav>
-          <Form className="d-flex flex-shrink-0" >
-            <Form.Control
-              type="search"
-              placeholder="레시피 또는 셰프"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="outline-success">검색</Button>
-          </Form>
+          <SearchBar />
         </Container>
       </Navbar>
 
