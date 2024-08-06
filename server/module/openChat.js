0fc6fe20b257e04fbe64f8db8a4639b04c26a119
ASDF110724 <twento157@gmail.com>
2024-08-06 17:55:08 +0900

    채팅 내역 불러오는거 까지는 가능 근데 랜더링이 느려서 채팅이 안뜸

--- server/module/openChat.js
+++ server/module/openChat.js
@@ -1,9 +1,25 @@
-function chatDataLog(data){
+async function chatDataLog(data){
     const maria = require('./sql');
-    const queryString = `INSERT INTO user_open_chat(user_code, chat_data) VALUES(?, ?)`
+    const queryString = `INSERT INTO user_open_chat(user_code, user_name, chat_data) VALUES(?, ?, ?)`
     // const queryString = `INSERT INTO user_open_chat(chat_data) VALUES(?)`
 
-    maria.execute(queryString,[data.id,data.message]);
+    await maria.execute(queryString,[data.id,data.name,data.message]);
 }
 
-module.exports = { chatDataLog };
+async function getChatLog(){
+    const maria = require('./sql');
+    const queryString = `SELECT user_code, user_name, chat_data
+                        FROM (
+                            SELECT *
+                            FROM user_open_chat
+                            ORDER BY chat_time DESC
+                            LIMIT 10
+                        ) AS subquery
+                        ORDER BY chat_time ASC`
+
+     let [chatlog] =  await maria.execute(queryString)
+
+    return chatlog;
+}
+
+module.exports = { chatDataLog , getChatLog };
