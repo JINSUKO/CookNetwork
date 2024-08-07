73623f3408f2ae471895a49aff4fc03dc842db4e
ASDF110724 <twento157@gmail.com>
2024-08-07 11:42:53 +0900

    08/07 소켓 코드리뷰 (#37)
    * 채팅 내역 불러오는거 까지는 가능 근데 랜더링이 느려서 채팅이 안뜸
    
    * 08/207 소켓 코드리뷰
    
    * 코드리뷰 약간 수정

--- client/src/chat/chatIndex.jsx
+++ client/src/chat/chatIndex.jsx
@@ -1,7 +1,9 @@
 import {useEffect, useRef, useState} from 'react';
 import {io} from 'socket.io-client';
 
+//작성해둔 컴포넌트 불러옴
 import {StyledApp} from './styled.jsx';
+
 
 const{
   Container,
@@ -15,19 +17,28 @@ const{
   MessageInput,
   SubmitButton
 } = StyledApp;
+
 const socket_IP  = import.meta.env.VITE_SOCKET_IP
 // const socket = new io('http://192.168.0.103:3001/'); // 관용님 학원 pc ip
+// 클라이언트 소켓 서버와 연결
 const socket = new io(socket_IP); 
 // const socket = new io('http://192.168.0.14:3001/'); // 진수님 학원 wifi ip
 
 function chatIndex({ userData }) {
+  // 유저 데이터를 받아와서 변수에 딕셔너리 형터로 저장
   const { user_code, user_id, username } = userData || {}
+  // 새로운 채팅 내역
   const [messageHistory, setMessageHistory] = useState([]);
+  // 최근 10개의 채팅내역
   const [oldMessageLog, setOldMessageLog] = useState([]);
 
+  // 변동 사항을 적용하기 위한 REF지정
   const uid = useRef(null);
   const historyElement = useRef(null);
 
+  // 유저가 전송하는 메세지 저장
+  // 처음 유저 데이터를 불러오는것에 실패해 undefind가 나오면 ''로 지정
+  // TODO 메세지는 빈 값일경우 경고창이 뜨도록 나중에 수정할것
   const [userMessage, setUserMessage] = useState({
     id: user_code || '',
     name: username || '',
@@ -35,31 +46,40 @@ function chatIndex({ userData }) {
   });
   
   useEffect(() => {
+    //[user_code, user_id, username] 변동시 (처음 접속시) 실행
     if(user_code && user_id && username){
+      // 클라이언트에 'USER_ENTER'이름으로 유저정보를 emit
       socket.emit('USER_ENTER',{ code: user_code, id: user_id, name: username});
+
       uid.current = user_code;
   }
 
+    // 서버에서 data를 받아 messageHistory에 저장
     const onMessage = (data) =>{
       console.log(`${data.id}: `,data);
       setMessageHistory((prevHistory) => [...prevHistory, data]);
     };
+    // 서버에서 user를 받아 출력
     const onUserEnter = (user) =>{
       console.log(`${user.name}(${user.id}) 접속`);
     };
+    // 서버에서 user를 받아 출력
     const onUserLeave = (user) =>{
       console.log(`${user.name}(${user.id}) 접속 해제`);
     };
+    // 서버에서 data를 받아 oldMessageLog에 저장
     const onChatLog = (data) =>{
       setOldMessageLog(data);
     }
     
+    // 서버에서 각각 emit 받을경우 실행
     socket.on('Message', onMessage);
     socket.on('USER_ENTER', onUserEnter);
     socket.on('USER_LEAVE', onUserLeave);
     socket.on('CHAT_LOG',onChatLog);
 
     return () => {
+      // 메모리 누수 방지용으로 종료시 서버와 연결 끊음
       socket.off('Message', onMessage);
       socket.off('USER_ENTER', onUserEnter);
       socket.off('USER_LEAVE', onUserLeave);
@@ -68,13 +88,17 @@ function chatIndex({ userData }) {
   }, [user_code, user_id, username]); 
 
   useEffect(() => {
+    // messageHistory,oldMessageLog에 변동값이 있을때 실행
     if(historyElement.current){
+      //scrollTop과 scrollHeight를 일치시켜 채팅이 늘어도 가려지는 부분 없이 전부 나오도록 함
       historyElement.current.scrollTop = historyElement.current.scrollHeight;
     }
     console.log(messageHistory)
   }, [messageHistory,oldMessageLog]);
 
   const handleChange = (event) =>{
+    // 메세지 입력창에서 실행
+    // 메세지를 입력할때마다 입력하는 내용이 바로 적용되도록 실행
     setUserMessage((prevState) => ({
       ...prevState,
       [event.target.id]: event.target.value,
@@ -82,12 +106,18 @@ function chatIndex({ userData }) {
   };
 
   const handleSubmit = (e) =>{
+    // 전송 방지
+    // 없을경우 Submit버튼 누를때 페이지 새로고침함
     e.preventDefault();
+    // 입력받은 메세지를 딕셔너리 형태로 저장
     const messageToSend = { id: uid.current, name: userMessage.name, message: userMessage.message };
+    // 서버에 'Message'이름으로 messageToSend를 emit
     socket.emit('Message', messageToSend);
 
+    // messageHistory에 저장
     setMessageHistory((prevHistory) => [...prevHistory, messageToSend]);
 
+    // 보낸후 userMessage의 message 내용을 초기화
     setUserMessage((prevUser) => ({
       ...prevUser,
       message: '',
@@ -97,10 +127,12 @@ function chatIndex({ userData }) {
   return (
     <Container>
       <HistoryWrapper ref={historyElement}>
+        {/* TODO 첫 로딩은 상관없으나 이후에 새로고침시 새로운 정보가 들어올때까지 Loading...이 뜸 해결방안 생각해보기  */}
       {oldMessageLog.length === 0 && messageHistory.length === 0 ? (
           <NoHistory>Loading...</NoHistory>
         ) : (
           <>
+          {/*  oldMessageLog,  messageHistory 내용을 반복해서 출력  */}
           {oldMessageLog.map(({ user_code, user_name, chat_data }, index) => (
             <ChatItem key={index} me={user_code === userData.user_code}>
               <ChatName>{user_name}</ChatName>
--- server/router/signUp.js
+++ server/router/signUp.js
@@ -3,9 +3,13 @@ const router = express.Router()
 
 const maria = require('../module/sql') ;
 
-router.post('/', async(req,res)=>{
-    const { userId, password, nickname, userSex, userEmail } = req.body;
 
+// 회원가입 라우터
+// 클라이언트부터 post요청을 받을시 실행
+router.post('/', async(req,res)=>{
+    // 클라이언트로부터 유저정보를 받음
+    const { userId, password, nickname, userSex, userEmail } = req.body;
+    // 클라이언트에서 받은 데이터를 기반으로 db에 INSERT 인젝션 공격 방지를 위해 값을 ?로 받음
     const queryString = `INSERT INTO users(user_id, user_password, username, sex, email) VALUES(?, ?, ?, ?, ?)`
     
     try{
