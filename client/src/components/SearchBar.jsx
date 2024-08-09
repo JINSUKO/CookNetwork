65da895893c2824d092e590ee00531755a1fb6dd
doy00 <164746857+doy00@users.noreply.github.com>
2024-08-09 19:01:25 +0900

    레시피 상세페이지, 검색 결과  데이터 호출 (#45)
    * 네비바, 검색 드롭다운 동적라우팅 수정
    
    * 검색창 관련 수정
    -SearchBar: 검색창 입력, 카테고리 선택시 이벤트 추가
    -SearchCategoryDrop: 드롭다운 메뉴 선택하여 검색시 url 변경(/category ->  /search)
    
    * 검색창 컴포넌트 내 기능별 함수 위치 정리
    
    * 불필요한 페이지 삭제
    
    * RecipeDetailPage 추가,
      handleSearch 함수 SearchBar로 이동
    
    * 카테고리별 검색기능 url 수정
    
    * 메인페이지 불러올 페이지 이름 RecipeListPage.jsx로 수정
    
    * 메인 레시피 리스트 호출 관련 수정
    
    * 레시피 상세 페이지 fetch 추가
    
    * 카테고리 url 변경(영어->한글)
    
    * SearchBar url 변경 ("/" => "?")
    카테고리 이름 변경 영어=> 한글
    
    * 검색결과 페이지 내 category 관련 수정- 전체 카테고리로 검색시 null 뜨는 문제 해결, handleFilter 함수 제거
    
    * 레시피 상세페이지 데이터 호출 및 화면 출력

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -3,15 +3,16 @@
 메인 상단 검색창, 버튼은 부트스트랩을 사용하였습니다.
 */
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { Button, Form } from "react-bootstrap";
 import SearchCategoryDrop from "./SearchCategoryDrop";
 import "../assets/styles/SearchBar.css"
 
-function SearchBar({ onSearch }) {    // onSearch를 prop으로 받기
-  const [searchInput, setSearchInput] = useState("") ;    // useState 사용하여 검색한 키워드 저장
-  const [selectedCategory, setSelectedCategory] = useState("all")
+function SearchBar({ onSearch, recipeData }) {  
+  const [searchInput, setSearchInput] = useState("") ; 
+  const [selectedCategory, setSelectedCategory] = useState("all");
+  const [results, setResults] = useState([]);
   const navigate = useNavigate();
 
   // onChange 함수 
@@ -28,13 +29,14 @@ function SearchBar({ onSearch }) {    // onSearch를 prop으로 받기
   // 검색을 수행하는 handleSearch 함수
   const handleSearch = () => {
     const searchPath = selectedCategory === 'all'
-      ? `/search/q=${encodeURIComponent(searchInput)}`   // 전체 카테고리 선택시
-      : `/search/q=${encodeURIComponent(searchInput)}&category=${encodeURIComponent(selectedCategory)}`;
+      ? `/search?q=${encodeURIComponent(searchInput)}`   // 전체 카테고리 선택시
+      : `/search?q=${encodeURIComponent(searchInput)}&category=${encodeURIComponent(selectedCategory)}`;
     
     navigate(searchPath);
     if (onSearch) {
       onSearch(searchInput, selectedCategory);
     }
+    // handleFilter()
   };
 
   // submit(검색) 버튼 클릭시 이벤트 함수
@@ -42,6 +44,26 @@ function SearchBar({ onSearch }) {    // onSearch를 prop으로 받기
     event.preventDefault();
     handleSearch();   // handleSearch를 호출하여 현재 검색어와 선택된 카테고리를 NavbarElement.jsx에 전달
   };
+  
+  /*
+  검색어를 필터링할 handleFilter.
+  -검색어와 같은 값들을 걸러내줄 filter 함수
+  -배열의 특정 요소가 포함되는지 판별해주는 include 함수
+  -toLocaleLowerCase 함수로 입력값과 제목을 소문자로 통일시킴. includes 함수는 대소문자를 인식(구분)하기 때문에 필요
+  -공백 유무 상관없이 검색 가능하게 해줄 replace 함수
+  */
+
+  // useEffect(() => {
+  //   handleFilter();
+  // }, [searchInput, recipeData]);
+
+  // const handleFilter = () => {
+  //   if (!recipeData) return;    // 데이터가 없을 때에는 함수 실행안함
+  //   const results = recipeData.filter((recipe) => recipe.recipe_name.replace(" ","").toLocaleLowerCase().includes(searchInput.toLocaleLowerCase().replace(" ",""))
+  //   )
+  //   setResults(results)
+  //   console.log("results:", results)
+  // };
 
 
   return(
