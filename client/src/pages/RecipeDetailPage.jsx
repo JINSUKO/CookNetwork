a3b814b5557a2b64d56b5295312af94ed48111ec
dhkim <dhkm201@gmail.com>
2024-08-09 14:45:31 +0900

    레시피 상세 페이지 fetch 추가

--- client/src/pages/RecipeDetailPage.jsx
+++ client/src/pages/RecipeDetailPage.jsx
@@ -5,38 +5,96 @@
 -동적라우팅 매개변수 recipe 
 */
 
-import React from 'react';
-import { Container } from 'react-bootstrap';
-import { useParams, useLocation, useNavigate } from 'react-router-dom';
+import React, { useState, useEffect, useCallback } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import { useParams, useNavigate } from 'react-router-dom';
 
-function RecipeDetailPage( recipe ) {
+function RecipeDetailPage(  ) {
   const { recipe_id } = useParams();
   // const location = useLocation();
   const navigate = useNavigate();
+  const [recipe, setRecipe] = useState([]);   // recipes 데이터 빈 배열로 설정
 
-  // 
-  // useEffect(() => {
-  //   if () {
+  const fetchRecipeDetails = useCallback(async () => {
+    try {
+      const response = await fetch(`/api/recipe/${recipe_id}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      if(!response.ok) {
+        throw new Error((await response.json()).error);
+      }
 
-  //     navigate("/recipe/:id");
-  //   }
-  // }, [])
+      // 레시피 데이터 result를 받아 recipes에 저장
+      const result = await response.json();
+      console.log("성공:", result)
+      if (result) {
+        console.log(`${recipe_id} 레시피 호출 성공`);
+        setRecipe(result.recipe);
+      }
+    } catch (error) {
+      console.error("레시피 호출 실패:", error);
+    }
+  }, [recipe_id]);
+  
+  useEffect(() => {   // 컴포넌트가 마운트될 때 fetch 함수 호출
+    fetchRecipeDetails();
+  }, [fetchRecipeDetails]);
+  
+
+
+  // function RecipeDetailPage(  ) {
+  //   const { recipe_id } = useParams();
+  //   // const location = useLocation();
+  //   const navigate = useNavigate();
+  //   const [recipe, setRecipe] = useState(null)
+    
+  //   useEffect(() => {
+  //     const fetchRecipeDetail = async () => {
+  //       try {
+  //         // const response = await fetch(`/api/recipes/${recipe_id}`);
+  //         // const data = await response.json();
+  //         // setRecipe(data);
+  //         setRecipe({
+  //           recipe_id,
+  //           recipe_name: "비빔밥",
+  //           recipe_img: "https://recipe1.ezmember.co.kr/cache/recipe/2015/12/08/0d2249438aac593752292c6380dbb5c41.jpg",
+  //           user_img: "https://recipe1.ezmember.co.kr/cache/rpf/2015/11/17/f06cd20689f5e1a6a2abcfe44abab09b1.jpg",
+  //           description: "우리나라의 전통음식 비빔밥 레시피입니다!",
+  //         });
+  //     } catch (error) {
+  //       console.error("레시피 호출 실패:", error);
+  //       }
+  //     };
+  //     fetchRecipeDetail();
+  //   }, [recipe_id, navigate]);
+  
+
+
+  
+  if (!recipe) {
+    return <div>로딩 중...</div>;
+  }
+
 
 
   return (
     <Container className='recipe-detail-container'>
-      <div className='recipeTitleContainer'>{recipe_name}</div>
-        <div className="col-md-6">
-          <img src={recipe.recipe_img} width="100%" />
-          <img src={user_img}/>
-        </div>
-        <div className="col-md-6">
+      <Row>
+        <Col md={6}>
+          <div className='recipe-title-wrap'>
+            <h2>{recipe.recipe_name}</h2>
+          </div>
+          <img src={recipe.recipe_img} alt="레시피 사진" className="img-fluid"/>
+          <img src={recipe.user_img} alt="셰프 프로필" className="mt-3 rounded-circle" style={{width: '50px', height: '50px'}}/>
+        </Col>
+        <Col md-6>
           <h4 className='pt-5'>{recipe.recipe_name}</h4>
-          <p></p>
-        </div>
-
-      <p>레시피 ID: {recipe_id}</p>
-      {/* 여기에 레시피 상세 정보를 표시하는 로직을 추가하세요 */}
+          <p>{recipe.description}</p>
+        </Col>
+      </Row>
     </Container>
   );
 }
