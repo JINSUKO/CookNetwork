b735dda86a73e487eda3e4b6546378ac08dd012f
dhkim <dhkm201@gmail.com>
2024-08-09 18:51:30 +0900

    레시피 상세페이지 데이터 호출 및 화면 출력

--- client/src/pages/RecipeDetailPage.jsx
+++ client/src/pages/RecipeDetailPage.jsx
@@ -6,18 +6,20 @@
 */
 
 import React, { useState, useEffect, useCallback } from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Image } from 'react-bootstrap';
 import { useParams, useNavigate } from 'react-router-dom';
 
-function RecipeDetailPage(  ) {
+function RecipeDetailPage() {
   const { recipe_id } = useParams();
   // const location = useLocation();
   const navigate = useNavigate();
-  const [recipe, setRecipe] = useState([]);   // recipes 데이터 빈 배열로 설정
+  const [recipe, setRecipe] = useState(null);   // recipes 데이터 빈 배열로 설정
+  const API_URL = import.meta.env.VITE_HOST_IP;
+
 
   const fetchRecipeDetails = useCallback(async () => {
     try {
-      const response = await fetch(`/api/recipe/${recipe_id}`, {
+      const response = await fetch(`${API_URL}/api/recipe/${recipe_id}`, {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json'
@@ -32,7 +34,7 @@ function RecipeDetailPage(  ) {
       console.log("성공:", result)
       if (result) {
         console.log(`${recipe_id} 레시피 호출 성공`);
-        setRecipe(result.recipe);
+        setRecipe(result);
       }
     } catch (error) {
       console.error("레시피 호출 실패:", error);
@@ -72,31 +74,39 @@ function RecipeDetailPage(  ) {
   //   }, [recipe_id, navigate]);
   
 
-
-  
   if (!recipe) {
     return <div>로딩 중...</div>;
+  } else {
+    return (
+      <Container className='recipe-detail-container' fluid="xl">
+        <Row className="justify-content-center">
+          <Col md={8} lg={8} xl={6}>
+            <img src={recipe.recipe_img} alt="레시피 사진" className="img-fluid"/>
+            <div className='recipe-title-wrap'>
+              <h2>{recipe.recipe_name}</h2>
+              <p>{recipe.recipe_desc}</p>
+
+            </div>
+          </Col>
+        </Row>
+        <Row className="text-center my-3">
+          <Col xs={6} md={4}>{recipe.serving}인분</Col>
+          <Col xs={6} md={4}>{recipe.cooked_time}분</Col>
+          <Col xs={6} md={4}>레벨 {recipe.level}</Col>
+        </Row>
+        <Row className="align-items-center">
+          <Col  xs="auto">
+            <Image src={recipe.user_img} alt="셰프 프로필" roundedCircle  width={50} height={50} />
+            <span>{recipe.username}</span>
+          </Col>
+        </Row>
+      </Container>
+    );
   }
 
 
 
-  return (
-    <Container className='recipe-detail-container'>
-      <Row>
-        <Col md={6}>
-          <div className='recipe-title-wrap'>
-            <h2>{recipe.recipe_name}</h2>
-          </div>
-          <img src={recipe.recipe_img} alt="레시피 사진" className="img-fluid"/>
-          <img src={recipe.user_img} alt="셰프 프로필" className="mt-3 rounded-circle" style={{width: '50px', height: '50px'}}/>
-        </Col>
-        <Col md-6>
-          <h4 className='pt-5'>{recipe.recipe_name}</h4>
-          <p>{recipe.description}</p>
-        </Col>
-      </Row>
-    </Container>
-  );
+  
 }
 
 export default RecipeDetailPage;
