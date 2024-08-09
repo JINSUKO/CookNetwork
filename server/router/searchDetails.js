c13414fe3077b2eca9b2917b29b984041ffe8db1
ASDF110724 <twento157@gmail.com>
2024-08-09 16:38:25 +0900

    카테고리별/ 디테일 검색 (#42)
    * 채팅 내역 불러오는거 까지는 가능 근데 랜더링이 느려서 채팅이 안뜸
    
    * 08/207 소켓 코드리뷰
    
    * 코드리뷰 약간 수정
    
    * db에서 카테고리 분류해서 클라이언트로 전송
    
    * 채팅 내역 수정/ 새로고침시 바로 불러올수 있도록
    
    * 카테고리별/ 디테일 검색
    
    ---------
    
    Co-authored-by: 고 진수(JINSU KO) <99123637+JINSUKO@users.noreply.github.com>

---
+++ server/router/searchDetails.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const path = require('path')
+const fs = require('fs')
+const router = express.Router()
+
+const maria = require('../module/sql') ;
+
+router.get('/',async (req,res)=>{
+    const searchData = req.query;
+    console.log(searchData)
+    if(req.query.category == 'null'){
+        const queryString = `SELECT * FROM recipe 
+            WHERE recipe_name LIKE ?`;
+        try{
+            const searchWord = '%' + searchData.q + '%'
+            const [recipes] = await maria.execute(queryString,[searchWord]);
+            console.log(searchWord)
+            const profileBasePath = path.join(__dirname, '../', 'uploads', 'recipes', 'thumbnail/');
+
+            const searchRecipes = await Promise.all(recipes.map(async (recipe) => {
+                let profilePic = fs.readFileSync(path.join(profileBasePath, recipe.recipe_img), 'base64');
+                profilePic = 'data:image/jpeg;base64,' + profilePic;
+                return { ...recipe, recipe_img: profilePic };
+            }));
+        
+            return res.json(searchRecipes);
+        } catch(error){
+            console.error(error);
+        }
+    }else{
+        const queryString = `SELECT * FROM recipe r
+            JOIN recipe_category rc 
+            ON r.recipe_id = rc.recipe_id 
+            JOIN categories c 
+            ON rc.category_id = c.category_id 
+            WHERE recipe_name LIKE ?`;
+            try{
+                const searchWord = '%' + searchData.q + '%'
+                const [recipes] = await maria.execute(queryString,[searchWord]);
+                console.log(searchWord)
+                const profileBasePath = path.join(__dirname, '../', 'uploads', 'recipes', 'thumbnail/');
+    
+                const searchRecipes = await Promise.all(recipes.map(async (recipe) => {
+                    let profilePic = fs.readFileSync(path.join(profileBasePath, recipe.recipe_img), 'base64');
+                    profilePic = 'data:image/jpeg;base64,' + profilePic;
+                    return { ...recipe, recipe_img: profilePic };
+                }));
+            
+                return res.json(searchRecipes);
+            } catch(error){
+                console.error(error);
+            }
+    }
+})
+
+module.exports = router;
