06322884afecf11be5d336279fceedaecf290665
고진수 <rhwkwk789@gmail.com>
2024-09-23 11:50:31 +0900

    admin recipes 서버 쪽 작성중

--- server/router/adminRouter.js
+++ server/router/adminRouter.js
@@ -49,13 +49,80 @@ router.get('/userCounts', async (req, res) => {
 // users
 router.get('/recipes', async (req, res) => {
 
-    const pageOptions = {recipePerPage: 1, page: 1}
+    const pageOptions = {};
+    const placeholderList = [];
 
     if (!isEmptyObject(req.query)) {
         console.log('/api/admin/recipes req.query', req.query);
-        pageOptions['recipePerPage'] = parseInt(req.query.recipePerPage);
-        pageOptions['page'] = parseInt(req.query.page);
+
+        const tmp = {...req.query};
+
+        Object.keys(tmp).forEach(key => {
+
+            if (Number(tmp[key])) {
+                pageOptions[key] = Number(tmp[key]);
+                placeholderList.push(Number(tmp[key]));
+                return;
+            }
+
+            pageOptions[key] = tmp[key];
+            placeholderList.push(tmp[key]);
+        })
+
+        pageOptions['recipePerPage'] = Number(tmp.recipePerPage);
+        pageOptions['page'] = Number(tmp.page);
     }
+
+    console.log(pageOptions)
+    console.log(placeholderList)
+
+    console.log(placeholderList.length - 2)
+
+    if (placeholderList.length - 2) {}
+
+    placeholderList[placeholderList.length - 1]
+
+    const placeholderList2 = [...placeholderList];
+
+    placeholderList2[placeholderList2.length - 1] = placeholderList.length - 1;
+
+    const sqltmp = `SELECT 
+                        r.recipe_id AS 'Recipe Id',
+                        u.username AS 'Author',
+                        u.user_id AS 'User Id',
+                        r.recipe_name AS 'Recipe Name',
+                        r.recipe_img AS 'Recipe Image',
+                        r.recipe_desc AS 'Recipe Description',
+                        GROUP_CONCAT(DISTINCT c.category_name ORDER BY c.category_id SEPARATOR ', ') AS 'Recipe Categories',
+                        GROUP_CONCAT(DISTINCT CONCAT(i.ingredient_name, ' (', ri.count, ' ', ri.ingredient_unit, ')') ORDER BY ri.ingredient_id SEPARATOR ', ') AS 'Recipe Ingredients',
+                        GROUP_CONCAT(DISTINCT CONCAT(ro.cooked_order, '. ', ro.order_desc) ORDER BY ro.cooked_order SEPARATOR '\n') AS 'Cooked Orders',
+                        r.cooked_time AS 'Cooked Time',
+                        r.serving AS 'Serving',
+                        r.level AS 'Level',
+                        r.tips AS 'Tips',
+                        r.create_post_date AS 'Created Date',
+                        r.lastupdate_post_date AS 'Last Updated Date',
+                        r.post_type AS 'Post Type',
+                        r.bookmark_count AS 'Bookmark Counts'
+                    FROM 
+                        recipe r
+                    JOIN 
+                        recipe_category rc ON r.recipe_id = rc.recipe_id
+                    JOIN 
+                        categories c ON rc.category_id = c.category_id
+                    JOIN 
+                        recipe_ingredient ri ON r.recipe_id = ri.recipe_id
+                    JOIN
+                        ingredient i ON ri.ingredient_id = i.ingredient_id
+                    JOIN
+                        recipe_orders ro ON r.recipe_id = ro.recipe_id
+                    JOIN 
+                        users u ON r.user_code = u.user_code
+                    GROUP BY 
+                        r.recipe_id
+                    LIMIT ? OFFSET ?;`;
+
+    // const [ data121 ] = (sql, [pageOptions.recipePerPage, pageOptions.recipePerPage * (pageOptions.page - 1)]);
 
     try {
         const sql = `SELECT 
@@ -94,7 +161,6 @@ router.get('/recipes', async (req, res) => {
                         r.recipe_id
                     LIMIT ? OFFSET ?;`;
 
-        console.log(pageOptions)
 
         const [ data ] = await maria.execute(sql, [pageOptions.recipePerPage, pageOptions.recipePerPage * (pageOptions.page - 1)]);
 
