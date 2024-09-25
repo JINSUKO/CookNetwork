3986ae505dd3baf4754b4d3e23e449bfb7d2d55d
ASDF110724 <twento157@gmail.com>
2024-09-25 11:49:13 +0900

    세부 페이지에 필요한 항목이 모두 뜨도록 추가

--- server/router/detailPage.js
+++ server/router/detailPage.js
@@ -12,17 +12,35 @@ router.get('/:recipe_id', async(req,res) =>{
     // 클라이언트로부터 요청받은 id를 저장
     const id = req.params.recipe_id;
     // 해당하는 recipe와 해당 recipe를 작성한 유저를 찾는 쿼리
-    const queryString = `SELECT r.*,u.user_code, u.username, u.user_img
+    const recipeQuery = `SELECT r.*,u.user_code, u.username, u.user_img
     FROM recipe r
     JOIN users u 
-    ON  r.user_code = u.user_code
-    WHERE r.recipe_id = ?;`;
+    ON r.user_code = u.user_code
+    WHERE r.recipe_id = ?`;
 
-    const [data] = await maria.execute(queryString,[id])
+    const ingredientQuery = `SELECT i.ingredient_name, ri.count,ingredient_unit
+    FROM recipe_ingredient ri
+    JOIN ingredient i
+    ON ri.ingredient_id = i.ingredient_id
+    WHERE ri.recipe_id = ?`
+
+    const orderQuery = `SELECT recipe_orders.cooked_order,recipe_orders.order_desc,recipe_orders.order_img
+    FROM recipe_orders
+    WHERE recipe_id = ?
+    `
+    const categoryQuery = `SELECT c.category_name
+    FROM categories c
+    JOIN recipe_category rc
+    ON c.category_id = rc.category_id
+    WHERE rc.recipe_id = ?
+    `
+
+    const [data] = await maria.execute(recipeQuery,[id])
     
     // 해당하는 recipe가 있을경우 실행
     if(data.length > 0){
         try{
+
             // ../uploads/recipes/thumbnail/ 경로 지정
             // const recipeImgPath = path.join(__dirname, '../', 'uploads', 'recipes', 'thumbnail/');
             // ../uploads/users/profile_images/ 경로 지정
@@ -42,7 +60,25 @@ router.get('/:recipe_id', async(req,res) =>{
                 // 찾은 데이터를 json형태로 리턴
                 return { ...data, recipe_img: recipePic, user_img: profilePic};
             };
-            const result = await recipePage(data[0]);
+            const recipeData = recipePage(data[0]);
+
+            const [ingredients,orders,categories] = await Promise.all([
+                maria.execute(ingredientQuery, [id]),
+                maria.execute(orderQuery, [id]),
+                maria.execute(categoryQuery, [id])
+            ]);
+    
+            // const [ingredients] = await maria.execute(ingredientQuery, [id]);
+            // const [orders] = await maria.execute(orderQuery, [id]);
+            // const [categories] = await maria.execute(categoryQuery, [id]);
+
+
+            const result = {
+                ...recipeData,
+                ingredients: ingredients[0],
+                orders: orders[0],
+                categories: categories[0]
+            }
 
             // 클라이언트로 response
             return res.json(result);
